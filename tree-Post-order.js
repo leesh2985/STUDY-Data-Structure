248137296dcfcf0e226890ba8db239a5d140398a
Seung Lee <leesh2985@naver.com>
2023-05-30 18:46:01 +0900

    비선형자료구조_후위 순회

--- tree-Post-order.js
+++ tree-Post-order.js
@@ -1,5 +1,6 @@
 // 후위 순회 방법: L -> R -> N 
 // 1. 왼쪽 서브 트리를 후위 순회한다. -> 2.오른쪽 서브 트리를 후위 순회한다. -> 3. 현재 노드를 방문한다.
+
 preorder(node)
 if node.left != null then preorder(node.left)
 if node.right != null then preorder(node.right)

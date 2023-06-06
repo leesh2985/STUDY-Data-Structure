2c6a307880184a5ff66e24cc7ef10e542f325f94
Seung Lee <leesh2985@naver.com>
2023-06-06 22:29:40 +0900

    수정

--- BFS(BrdadthFirstSearch).mjs
+++ BFS(BrdadthFirstSearch).mjs
@@ -2,6 +2,33 @@
 
 import { Queue } from "./BFS_queue.mjs";
 
+function Graph() {
+  this.edge = {};
+  this.visited = {};
+}
+
+Graph.prototype.addVertex = function (v) {
+  this.edge[v] = [];
+  this.visited[v] = false;
+};
+
+Graph.prototype.addEdge = function (v1, v2) {
+  this.edge[v1].push(v2);
+};
+
+Graph.prototype.print = function () {
+  for (let vertex in this.edge) {
+    let neighbors = this.edge[vertex];
+    if (neighbors.length === 0) continue;
+
+    process.stdout.write(`${vertex} -> `);
+    for (let j = 0; j < neighbors.length; j++) {
+      process.stdout.write(`${neighbors[j]} `);
+    }
+    console.log("");
+  }
+};
+
 // bfs(): BFS 탐색
 Graph.prototype.bfs = function (startVertex) {
   this._bfsLoopVisit(startVertex);
@@ -34,3 +61,17 @@ let vertices = ["A", "B", "C", "D", "E", "F", "G", "H", "I"];
 for (let i = 0; i < vertices.length; i++) {
   graph.addVertex(vertices[i]);
 }
+
+graph.addEdge("A", "B");
+graph.addEdge("A", "C");
+graph.addEdge("A", "D");
+graph.addEdge("C", "G");
+graph.addEdge("D", "G");
+graph.addEdge("D", "H");
+graph.addEdge("B", "E");
+graph.addEdge("B", "F");
+graph.addEdge("E", "I");
+graph.print();
+console.log("");
+
+graph.bfs("A");
